7f5591de1a0c219206c86c5b609fcdea53e9442c
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-14 14:10:39 +0530

    Initial Commit

---
+++ src/main/java/com/deloitte/Test12456Application.java
@@ -0,0 +1,13 @@
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test12456Application.class, args);
+	}
+
+}
